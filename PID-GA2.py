d21e3c61621c014015dea3eb44d721e0c308965e
Christiantk02 <143506438+Christiantk02@users.noreply.github.com>
2025-02-19 23:40:14 +0100

    Update PID-GA2.py

--- PID-GA2.py
+++ PID-GA2.py
@@ -6,7 +6,7 @@ import random as rnd
 def system():
     # Generate a transferfunction for the system G(s) = num/den
     num = [1]
-    den = [1,1]
+    den = [1,2,1]
     sys = ctrl.TransferFunction(num,den)
     return sys
 
@@ -31,11 +31,51 @@ def plot(t, y):
     plt.show()
 
 def fitness(kp, ki, kd, system):
+    # Find fitness based on step response
     t, y = simulatePid(kp, ki, kd, system)
 
-    overshoot = max(y) -1 if max(y) > 1 else 0
+    overshoot = (max(y) -1)*100 if max(y) > 1 else 0
 
     steadyState = abs(y[-1] - 1)
 
-    
+    settlingTime = t[-1]
+
+    idx = np.where(abs(y-1) < 0.02)[0]
+    if len(idx) > 0:
+        for i in range(len(idx)):
+            if np.all(abs(y[idx[i]:] - 1) < 0.02):
+                settlingTime = t[idx[i]]
+                break
+
+    return 0.1*overshoot + steadyState + 0.1*settlingTime
+
+
+def initializePopulation(size):
+    # Initialize population with random values
+    population = []
+    for i in range(size):
+        kp = rnd.uniform(0, 5)
+        ki = rnd.uniform(0, 3)
+        kd = rnd.uniform(0, 1)
+        population.append((kp, ki, kd))
+    return population
+
+def evaluatePopulation(population, system):
+    # Evaluate population based on fitness
+    fitnessValues = []
+    for i in range(len(population)):
+        kp, ki, kd = population[i]
+        fitnessValues.append((fitness(kp, ki, kd, system), (kp, ki, kd)))
+    fitnessValues.sort(key=lambda x: x[0])  # ikke peiling  
+    return fitnessValues
+
+def selectParents(population, fitnessValues):
+    # Select parents based on fitness
+    parents = []
+    for i in range(len(population)//3):
+        parents.append(fitnessValues[i][1])
+    return parents
+
+
+ 
 
