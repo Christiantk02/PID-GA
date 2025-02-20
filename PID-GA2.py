bec308921e9ea5a96ab5e7f3651e08d74f60b35f
Christiantk02 <143506438+Christiantk02@users.noreply.github.com>
2025-02-20 18:30:17 +0100

    Finished

--- PID-GA2.py
+++ PID-GA2.py
@@ -41,13 +41,13 @@ def fitness(kp, ki, kd, system):
     settlingTime = t[-1]
 
     idx = np.where(abs(y-1) < 0.02)[0]
-    if len(idx) > 0:
+    if (len(idx) > 0):
         for i in range(len(idx)):
             if np.all(abs(y[idx[i]:] - 1) < 0.02):
                 settlingTime = t[idx[i]]
                 break
 
-    return 0.1*overshoot + steadyState + 0.1*settlingTime
+    return overshoot**overshoot + steadyState*3 + settlingTime*0.3
 
 
 def initializePopulation(size):
@@ -66,7 +66,7 @@ def evaluatePopulation(population, system):
     for i in range(len(population)):
         kp, ki, kd = population[i]
         fitnessValues.append((fitness(kp, ki, kd, system), (kp, ki, kd)))
-    fitnessValues.sort(key=lambda x: x[0])  # ikke peiling  
+    fitnessValues.sort(key=lambda x: x[0])  # Made By ChatGPT  
     return fitnessValues
 
 def selectParents(population, fitnessValues):
@@ -75,6 +75,65 @@ def selectParents(population, fitnessValues):
     for i in range(len(population)//3):
         parents.append(fitnessValues[i][1])
     return parents
+
+def crossover(parents, size):
+    # Crossover parents to make new children using Arithmetic metod
+    newPopulation = []
+    newPopulation.append(parents[0])
+
+    while (len(newPopulation) < size):
+        alpha = rnd.uniform(0,1)
+        p1, p2 = rnd.sample(parents, 2)
+        kp = alpha*p1[0] + (1-alpha)*p2[0] 
+        ki = alpha*p1[1] + (1-alpha)*p2[1] 
+        kd = alpha*p1[2] + (1-alpha)*p2[2] 
+        newPopulation.append((kp, ki, kd))
+    return newPopulation
+
+def mutate(population, mutationRate):
+    # Mutate population
+    newPopulation = []
+    newPopulation.append(population[0])
+
+    for i in range(len(population)-1):
+        individual = list(population[i])  
+        if rnd.random() < mutationRate:
+            individual[0] += rnd.uniform(-0.8, 0.8)  
+        if rnd.random() < mutationRate:
+            individual[1] += rnd.uniform(-0.4, 0.4)  
+        if rnd.random() < mutationRate:
+            individual[2] += rnd.uniform(-0.05, 0.05)  
+        newPopulation.append(tuple(individual))  
+    return newPopulation
+
+def geneticAlgorithm(system, populationSize, generations, mutationRate):
+    # Run genetic algorithm
+    population = initializePopulation(populationSize)
+
+    for i in range(generations):
+        fitnessValues = evaluatePopulation(population, system)
+
+        parents = selectParents(population, fitnessValues)
+
+        newPopulation = crossover(parents, populationSize)
+
+        population = mutate(newPopulation, mutationRate)
+
+        bestFitness, bestPid = fitnessValues[0]
+        print(f'Generation {i}: Best fitness: {bestFitness}, Best PID: {bestPid}')
+
+    return bestPid
+
+sys = system()
+bestP, bestI, bestD = geneticAlgorithm(sys,100, 100, 0.150)
+
+print(f'Best PID: P={bestP}, I={bestI}, D={bestD}')
+plot(*simulatePid(bestP, bestI, bestD, sys))
+        
+
+
+
+
 
 
  
